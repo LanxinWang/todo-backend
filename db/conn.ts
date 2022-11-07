a3946455487dc717d3a7670432af898462957547
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-11-07 15:43:50 +0800

    get ATLAS_URI

--- db/conn.ts
+++ db/conn.ts
@@ -1,11 +1,11 @@
-import { MongoClient,ConnectOptions, Db, Collection } from "mongodb"
+import { MongoClient, Collection } from "mongodb"
 import dotenv from "dotenv"
 import { Todo } from "../types";
 
 dotenv.config({path: "./config.env"});
-const connectionString = process.env.ATLAS_URI||'';
+const {ATLAS_URI} = process.env;
 
-const client = new MongoClient(connectionString);
+const client = new MongoClient(ATLAS_URI!);
 
 const db = client.db("todoData");
 const todoCollection: Collection<Todo> = db.collection('todoTasks');
