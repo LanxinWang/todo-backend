58523e52ee20a3ac9b1df0166fa211b91ac791ff
Lanxin Wang <lanxin.wang@thoughtworks.com>
2022-11-10 23:42:44 +0800

    refactor get all todos controller

--- controller/todoController.interface.ts
+++ controller/todoController.interface.ts
@@ -3,9 +3,9 @@ import { TodoService } from "../service/todoService";
 
 export interface todoControllerInterface {
     getAllTodos(req: Request, res: Response): void;
-    createTodo(req: Request, res: Response): void;
-    updateTodoById(req: Request, res: Response): void;
-    updateAllTodos(req: Request, res: Response): void;
-    deleteTodoById(req: Request, res: Response): void;
-    deleteAllCompletedTodos(req: Request, res: Response): void;
+    // createTodo(req: Request, res: Response): void;
+    // updateTodoById(req: Request, res: Response): void;
+    // updateAllTodos(req: Request, res: Response): void;
+    // deleteTodoById(req: Request, res: Response): void;
+    // deleteAllCompletedTodos(req: Request, res: Response): void;
 }
--- controller/todoController.ts
+++ controller/todoController.ts
@@ -1,4 +1,4 @@
-import todoCollection from "../db/conn"
+// import todoCollection from "../db/conn"
 import { Request, Response } from "express";
 import { ITodo } from "../types";
 import {todoControllerInterface} from "./todoController.interface"
@@ -20,60 +20,60 @@ export class TodoController implements todoControllerInterface {
         });
     }
 
-    public createTodo = (req: Request, res: Response) => {
-        const todo: ITodo = req.body.todo ;
-        this._todoService
-        .createTodo(todo)
-        .then(() => {
-        res.json(todo);
-        })
-        .catch(() => {
-        res.status(400).send("Error creating todo!");
-        });
-    }
+    // public createTodo = (req: Request, res: Response) => {
+    //     const todo: ITodo = req.body.todo ;
+    //     this._todoService
+    //     .createTodo(todo)
+    //     .then(() => {
+    //     res.json(todo);
+    //     })
+    //     .catch(() => {
+    //     res.status(400).send("Error creating todo!");
+    //     });
+    // }
 
-    public updateTodoById = (req: Request, res: Response) => {
-        const _id = Number(req.params.id);
-        const { isChecked } = req.body;
-        this._todoService.updateTodoById(_id, isChecked)
-            .then((result) => {
-            res.json(result);
-            })
-            .catch(() => {
-            res.status(400).send("Error update todo status!");
-            });
-    }
+    // public updateTodoById = (req: Request, res: Response) => {
+    //     const _id = Number(req.params.id);
+    //     const { isChecked } = req.body;
+    //     this._todoService.updateTodoById(_id, isChecked)
+    //         .then((result) => {
+    //         res.json(result);
+    //         })
+    //         .catch(() => {
+    //         res.status(400).send("Error update todo status!");
+    //         });
+    // }
     
-    public updateAllTodos = (req: Request, res: Response) => {
-        const { isChecked } = req.body;
-        this._todoService.updateAllTodos(isChecked)
-        .then((result) => {
-        res.json(result);
-        })
-        .catch(() => {
-        res.status(400).send("Error update todos status!");
-        });
-    }
+    // public updateAllTodos = (req: Request, res: Response) => {
+    //     const { isChecked } = req.body;
+    //     this._todoService.updateAllTodos(isChecked)
+    //     .then((result) => {
+    //     res.json(result);
+    //     })
+    //     .catch(() => {
+    //     res.status(400).send("Error update todos status!");
+    //     });
+    // }
 
-    public deleteTodoById = (req: Request, res: Response) => {
-        const _id = Number(req.params.id);
-        this._todoService.deleteTodoById(_id)
-            .then((result) => {
-            res.json(result);
-            })
-            .catch(() => {
-            res.status(400).send("Error delete todo!");
-            });
-    }
+    // public deleteTodoById = (req: Request, res: Response) => {
+    //     const _id = Number(req.params.id);
+    //     this._todoService.deleteTodoById(_id)
+    //         .then((result) => {
+    //         res.json(result);
+    //         })
+    //         .catch(() => {
+    //         res.status(400).send("Error delete todo!");
+    //         });
+    // }
 
-    public deleteAllCompletedTodos = (req: Request, res: Response) => {
-        this._todoService.deleteAllCompletedTodos()
-            .then((result) => {
-            res.json(result);
-            })
-            .catch(() => {
-            res.status(400).send("Error delete all completed todos!");
-            });
-    }
+    // public deleteAllCompletedTodos = (req: Request, res: Response) => {
+    //     this._todoService.deleteAllCompletedTodos()
+    //         .then((result) => {
+    //         res.json(result);
+    //         })
+    //         .catch(() => {
+    //         res.status(400).send("Error delete all completed todos!");
+    //         });
+    // }
 
 }
